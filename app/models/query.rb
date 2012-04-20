adcfee4139f70705873034b788cc254cf440df58
David De Groot <daviddegroot@David-De-Groots-MacBook-Air>
2012-04-20 18:35:34 -0400

    commit after a little bit of work

--- app/models/query.rb
+++ app/models/query.rb
@@ -7,51 +7,54 @@ class Query < ActiveRecord::Base
   protected
   def get_first_script
     @testcase = self.testcase
-    @first_script = Script.where(:testcase_id => @testcase.id).first
+    @first_script = Script.where(:testcase_id => @testcase.id).first.id
     parse_script_add_parameters_and_run(@first_script)
   end
   
   protected
   def parse_script_add_parameters_and_run(script_id)
-      @script = s.script.where(:id => script_id)
+      scriptmodel = Script.where(:id => script_id)
+      @script = scriptmodel.script
       case @script
       #parse the domain name from the email address
       when /~domain/
-            d= self.DirectAddress.partition("@")[2]
+            d = self.DirectAddress.partition("@")[2]
             #add the domain name to the script
-            @updated_script=@script.sub("~domain",d)
-            run_script(@updated_script, s.expectedResult, s.id+1)
+            @updated_script = @script.sub("~domain",d)
+            run_script(@updated_script, @script.expectedResult, @script.id+1)
       when /~email/
-            e= self.DirectAddress
+            e = self.DirectAddress
             #add the email address to the script
-          @updated_script=@script.sub("~email",e)
-          run_script(@updated_script, s.expectedResult, s.id+1)
+          @updated_script = @script.sub("~email",e)
+          run_script(@updated_script, @script.expectedResult, @script.id+1)
       when /~dnsemail/
           #add the email address formatted as a domain name to the script
-          d= self.DirectAddress.sub("@",".")
-          @updated_script=@script.sub("~dnsemail",d)
-          run_script(@updated_script, s.expectedResult, s.id+1)
-        else self.pass = false  
-        end
+          d = self.DirectAddress.sub("@",".")
+          @updated_script = @script.sub("~dnsemail",d)
+          run_script(@updated_script, @script.expectedResult, @script.id+1)
+      when /~last_response/
+          last_scriptmodel = Script.where(:id => scriptmodel.id-1)
+          last_expected_result = last_scriptmodel.expectedResult
+          @updated_script = @script.sub("~last_response", @script.expectedResult)
+          run_script(@updated_script, @script.expectedResult, @script.id+1)
+      else self.pass = false  
       end
-    end
   end
   
 protected
   def run_script(script, expected_result, next_script_id)
     until !self.pass.nil?
-    ran_script=%x[#{script}] 
-    @next_script = Script.where(:id => next_script_id)
+      @next_script = Script.where(:id => next_script_id)
+      ran_script = %x[#{script}] 
         if ran_script.include?(expected_result)
-          if @next_script.empty?
-            self.pass = true
-            else parse_script_and_add_parameters(next_script.id)
-            end
-
-            self.pass = true
-            else
-            self.pass = false
+          if @next_script.script.empty?
+          self.pass = true
+          else parse_script_and_add_parameters(next_script.id)
+          end
+        else
+          self.pass = false
         end
+    end
   end
   
   protected
